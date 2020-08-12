df21dad6f50f039b6e139094e0bc64a9af90fd00
Jean Amadeu <jean.amadeu2@gmail.com>
2020-08-12 06:26:30 -0300

    start

---
+++ src/main/java/br/com/jamadeu/springdatajpa/ExemploSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.jamadeu.springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jamadeu/springdatajpa/ExemploSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jamadeu.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
