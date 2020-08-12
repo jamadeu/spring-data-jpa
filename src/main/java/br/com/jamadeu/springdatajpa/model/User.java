c8aa2e10296b2e0b976b1ba1836ebed21679b471
Jean Amadeu <jean.amadeu2@gmail.com>
2020-08-12 09:07:28 -0300

    Create model

---
+++ src/main/java/br/com/jamadeu/springdatajpa/model/User.java
@@ -0,0 +1,82 @@
+package br.com.jamadeu.springdatajpa.model;
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import java.util.Objects;
+
+/**
+ * @author Jean Amadeu 08/12/2020
+ */
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    @Email
+    private String email;
+
+    public User() {
+    }
+
+    public User(@NotEmpty String name, @NotEmpty @Email String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return getId().equals(user.getId()) &&
+                getName().equals(user.getName()) &&
+                getEmail().equals(user.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getEmail());
+    }
+}
