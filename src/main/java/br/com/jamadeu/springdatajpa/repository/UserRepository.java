fc25f327bbf5c5dcee6b4cf2852ce8b629d54542
Jean Amadeu <jean.amadeu2@gmail.com>
2020-08-13 20:16:47 -0300

    finish

--- src/main/java/br/com/jamadeu/springdatajpa/repository/UserRepository.java
+++ src/main/java/br/com/jamadeu/springdatajpa/repository/UserRepository.java
@@ -1,8 +1,8 @@
 package br.com.jamadeu.springdatajpa.repository;
 
 import br.com.jamadeu.springdatajpa.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,7 +13,7 @@ import java.util.Optional;
  */
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByName(String name);
 
     List<User> findByEmailEndingWith(String domain);
